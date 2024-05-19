db2bda9abbccf5fe58a053f0df60815d14987910
likeca <like.ca@gmail.com>
2024-05-19 02:26:06 +0000

    Deploying to gh-pages from @ likeca/egorders-fe@2b1bcf5577604f976e537b441e7bdb266c9cd3c0 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "121501203"
+    serviceWorkerVersion: "3763234283"
   }
 });
